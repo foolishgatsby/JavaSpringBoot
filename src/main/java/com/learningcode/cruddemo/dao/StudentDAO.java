2ed20959dcf1bc015f7a0261b2a4720747d79a68
ThanhThe <thehoang13102001@gmail.com>
2023-12-18 16:26:48 +0700

    creat Student object, StudentDAO interface and StudentDAOImpl to help access to DB and save it

---
+++ src/main/java/com/learningcode/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,12 @@
+/**
+ * Student Database Access Object
+ */
+
+package com.learningcode.cruddemo.dao;
+
+import com.learningcode.cruddemo.entity.Student;
+
+public interface StudentDAO {
+
+    void save(Student theStudent);
+}
---
+++ src/main/java/com/learningcode/cruddemo/dao/StudentDAOImpl.java
@@ -0,0 +1,28 @@
+package com.learningcode.cruddemo.dao;
+
+import com.learningcode.cruddemo.entity.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentDAOImpl implements StudentDAO{
+
+    // define field for entity manager
+    private EntityManager entityManager;
+
+    // inject entity manager using constructor injection
+    @Autowired
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    // implement save method
+    @Override
+    @Transactional
+    public void save(Student theStudent) {
+        // save the student to database
+        entityManager.persist(theStudent);
+    }
+}
